b4abfea7430595c5c82b2b630f9aff2d61da0cc1
LesterCuasay <116585764+LesterCuasay@users.noreply.github.com>
2023-11-22 01:20:53 +0000

    feat - added media queries for breakpoints

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,51 @@
+/* Toggle Menu Icon */
+
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
+
+/* Toggle Menu Icon */
+
 /* Scroll section active links */
 
-document.addEventListener("DOMContentLoaded", function() {
-    const navLinks = document.querySelectorAll('.navbar a');
+document.addEventListener("DOMContentLoaded", function () {
+  const navLinks = document.querySelectorAll(".navbar a");
 
-    function updateActiveClass() {
-      const scrollPosition = window.scrollY;
+  function updateActiveClass() {
+    const scrollPosition = window.scrollY;
 
-      navLinks.forEach(link => {
-        const targetId = link.getAttribute('href').substring(1);
-        const targetSection = document.getElementById(targetId);
+    navLinks.forEach((link) => {
+      const targetId = link.getAttribute("href").substring(1);
+      const targetSection = document.getElementById(targetId);
 
-        if (targetSection) {
-          const sectionTop = targetSection.offsetTop - 150;
-          const sectionHeight = targetSection.offsetHeight;
+      if (targetSection) {
+        const sectionTop = targetSection.offsetTop - 150;
+        const sectionHeight = targetSection.offsetHeight;
 
-          if (scrollPosition >= sectionTop && scrollPosition < sectionTop + sectionHeight) {
-            link.classList.add('active');
-          } else {
-            link.classList.remove('active');
-          }
+        if (
+          scrollPosition >= sectionTop &&
+          scrollPosition < sectionTop + sectionHeight
+        ) {
+          link.classList.add("active");
+        } else {
+          link.classList.remove("active");
         }
-      });
-    }
+      }
+    });
+    /* Sticky Navbar */
 
-    window.addEventListener('scroll', updateActiveClass);
-    updateActiveClass();
-  });
+    let header = document.querySelector("header");
+
+    header.classList.toggle("sticky", window.scrollY > 100);
+
+    menuIcon.classList.remove("bx-x");
+    navbar.classList.remove("active");
+  }
+
+  window.addEventListener("scroll", updateActiveClass);
+  updateActiveClass();
+});
